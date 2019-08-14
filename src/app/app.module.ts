c06fc6c92b689cb819584c5c77abe6f47c1292e0
bcParker <cparker8890@gmail.com>
2019-08-14 17:47:28 -0400

    CP: Wonky product rows

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import { FormsModule } from '@angular/forms';
     MaterialModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule,
+    HttpClientModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
